9fcf4096c5fd3f4d2c9560c2d7a9b9c434544e6c
rohitmehlawat <rohitmehlawat27@gmail.com>
2018-06-20 10:42:54 +0530

    payment mode value changed

--- steppers/paymentMode.js
+++ steppers/paymentMode.js
@@ -7,7 +7,7 @@ exports.getPaymentMode=function(req,res,next){
     if(res.locals.executeInstrumentParam) {
 
         var SPParamters=res.locals.SPParameters;
-        var paymentModeRef = req.body.charges.PaymentModeParam;
+        var paymentModeRef = req.body.charges;
         if(paymentModeRef===undefined){
             logger.log('error',"error in getPaymentMode, doesnot contain the required field paymentModeRef");
             req.headers.statusCode="E00002";
@@ -15,9 +15,9 @@ exports.getPaymentMode=function(req,res,next){
             res.send(response);
             return;
         }
-
-        if (paymentModeRef > 0) {
-            laasRepository.getPaymentMode(paymentModeRef)
+        var paymentAmount=paymentAmount(paymentModeRef);
+        if (paymentAmount > 0) {
+            laasRepository.getPaymentMode(paymentAmount)
                 .then((result)=>{
                     logger.log('info',"in paymentMode Result ------>>>>>"+ JSON.stringify(result));
                     try{
@@ -46,4 +46,42 @@ exports.getPaymentMode=function(req,res,next){
         }
     }
   next();
+};
+
+var paymentAmount=(charges)=>{
+    if(charges!==undefined){
+        if(charges.hasOwnProperty(cstotalamt) && charges.cstotalamt>0){
+            return charges.cstotalamt;
+        }
+        else if(charges.hasOwnProperty(cqtotalamt) && charges.cqtotalamt>0){
+                return charges.cqtotalamt;
+        }
+        else if(charges.hasOwnProperty(cctotalamt) && charges.cctotalamt>0){
+            return charges.cctotalamt;
+        }
+        else if(charges.hasOwnProperty(dctotalamt) && charges.dctotalamt>0){
+            return charges.dctotalamt;
+        }
+        else if(charges.hasOwnProperty(actotalamt) && charges.actotalamt>0){
+            return charges.actotalamt;
+        }
+        else if(charges.hasOwnProperty(optotalamt) && charges.optotalamt>0){
+            return charges.optotalamt;
+        }
+        else if(charges.hasOwnProperty(kntotalamt) && charges.kntotalamt>0){
+            return charges.kntotalamt;
+        }
+        else if(charges.hasOwnProperty(dftotalamt) && charges.dftotalamt>0){
+            return charges.dftotalamt;
+        }
+        else if(charges.hasOwnProperty(wltotalamt) && charges.wltotalamt>0){
+            return charges.wltotalamt;
+        }
+        else{
+            return 0;
+        }
+    }
+    else{
+        return 0;
+    }
 };
